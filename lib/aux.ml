c6cb2e5ba1388bb9a031d382dea29abb22990084
Takezoe,Tomoaki <tomoaki3478@res.ac>
2019-12-28 16:24:04 +0900

    Parse test estoril files

--- lib/aux.ml
+++ lib/aux.ml
@@ -3,3 +3,15 @@
  * SPDX-Identifier: Apache-2.0 WITH LLVM-exception
  *)
 
+let walk_dir_handle dir f =
+  try
+    while true do
+      Unix.readdir dir |> f
+    done
+  with End_of_file -> ()
+
+let walk_dir path f =
+  let dir = Unix.opendir path in
+  walk_dir_handle dir f;
+  (* TODO: finally *)
+  Unix.closedir dir
--- test/test_lexer.ml
+++ test/test_lexer.ml
@@ -2,34 +2,15 @@
  *
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  *)
-open OUnit2
 open Aux
+open Estoril.Aux
 open Estoril
 
 let _ =
-  begin_suite "Estoril.Lexer";
-  test "lex" (fun _ ->
-      let check source =
-        Printf.printf "lexing '%s'\n" source;
-        let { Lexer.read; _ } = Estoril.Lexer.from_string source in
-        let rec loop () =
-          match read () with
-          | Estoril.Token.EOF _ -> ()
-          | t ->
-              Printf.printf "%s\n" (Token.show_token t);
-              loop ()
-        in
-        loop ()
-      in
-      let lines =
-        [
-          "# header"; "This is a *emphasized string*, *emphasized string2*, and *emphasized string3*."; "## header2"; "";
-        ]
-      in
-      let source = String.concat "\n" lines in
-      check source;
-      let source = "test" in
-      Estoril.Lexer.parse Estoril.Parser.document (Estoril.Lexer.from_string source)
-      |> Syntax.show_document |> Printf.printf "%s\n";
-      assert_equal 0 0);
+  begin_suite "parse EstorilMarkdown";
+  walk_dir "." (fun path ->
+      if BatString.ends_with path ".estrl" then
+        test path (fun _ ->
+            Lexer.from_filename path |> Lexer.parse Parser.document |> Syntax.show_document |> Printf.printf "%s\n";
+            Printf.printf "%s\n" path));
   end_suite ()
